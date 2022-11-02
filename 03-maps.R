62ca3268110fe35e86afadf9c1121d8351e2853c
luehkenecology <luehken65@gmail.com>
2022-11-02 14:11:16 +0100

    read pa file from output folder

--- 03-maps.R
+++ 03-maps.R
@@ -3,3 +3,9 @@ RPROJ <- list(PROJHOME = normalizePath(getwd()))
 attach(RPROJ)
 rm(RPROJ)
 setwd(PROJHOME)
+
+# read table
+df <- read.table("output/mosquitoes_ger_pa.csv", 
+                 sep = ";", header = T, row.names = 1)
+
+
