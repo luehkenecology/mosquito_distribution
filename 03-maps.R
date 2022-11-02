b7b05705c5db3a88352e894ca1cb04ad568e162d
luehkenecology <luehken65@gmail.com>
2022-11-02 20:11:35 +0100

    read gadm-map Germany

--- 03-maps.R
+++ 03-maps.R
@@ -4,8 +4,20 @@ attach(RPROJ)
 rm(RPROJ)
 setwd(PROJHOME)
 
+# libraries
+library(geodata)
+
 # read table
 df <- read.table("output/mosquitoes_ger_pa.csv", 
                  sep = ";", header = T, row.names = 1)
+
+# map germany
+germany <- getData('GADM', country='DEU', level=1)
+
+# 
+plot(germany)
+
+
+
 
 
